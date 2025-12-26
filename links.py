eed49a727ecb38c8614d3ca9931901911e7653c5
syleam88888 <syleam8@gmail.com>
2025-12-26 16:15:16 +0100

    Create links.py

---
+++ links.py
@@ -0,0 +1,4 @@
+IMMOWEB_LINKS_VENTE = {
+    "Maman": "https://www.immoweb.be/fr/carte/maison-et-appartement/a-vendre?countries=BE&geoSearchAreas=uwnrHed~Z{D{_C|Ky~FuCwzDgNa{AhAexEe[ojAacAfv@k`Bx@_|@|y@}cE`U_jAkF_qAb`@kOpOe@vp@jOng@jAdv@kj@niE{RjsD?bcAbb@jsD|YrZjj@fSr^re@l_AxX`qAdHpy@yXz_@?nfA`m@zf@dHpd@?xs@m\\lq@kwA~t@c`@&page=1&orderBy=relevance",
+    "Autour de Namur": "https://www.immoweb.be/fr/carte/maison-et-appartement/a-vendre?countries=BE&geoSearchAreas=ysurHiom\\zh@m{AhQ}jAbLclG?ulCg{@ocI{Ww`AgyAi~BmwDiuA_eBuYmk@?ehAhp@}qDnkD{q@xnCef@vfH?ddE|q@~bFxu@`tBv{AtlCj~AfsAlkB`h@jqA`Az{Aw[dnBgzBtvAcoAjMox@uNtY&page=1&orderBy=relevance"
+}
